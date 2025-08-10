f6468ac815a6f42c4135a992750a45059bf61c3a
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-10 22:09:55 +0530

    Time: 6 ms (9.52%), Space: 63 MB (47.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,7 +5,9 @@ public:
         int n=height.size();
         int i=0, j=n-1;
         while(i<j){
+        //choosing the minimum height to store the water efficiently
         int Area=min(height[i], height[j])*(j-i);
+        //moving the shorter height so that area might increase
         maxArea=max(maxArea, Area);
         if(height[i]<height[j])i++;
         else j--;
