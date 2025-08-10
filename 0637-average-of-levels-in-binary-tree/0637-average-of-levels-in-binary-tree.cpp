725c950b4400d2a0b6579f987e489a2056c76b0b
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-10 16:33:28 +0530

    Time: 0 ms (100%), Space: 23.7 MB (94.17%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -12,23 +12,24 @@
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double>res;
-        if(root==nullptr)return res;
+        vector<double>result;
+        if(!root)return result;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             int size=q.size();
-            double levelSum=0;
+            double levelsum=0;
             for(int i=0; i<size; i++){
                 TreeNode* node=q.front();
                 q.pop();
-                levelSum+=node->val;
+                levelsum+=node->val;
                 if(node->left)q.push(node->left);
                 if(node->right)q.push(node->right);
-
             }
-            res.push_back(levelSum/size);
+            result.push_back(levelsum/size);
+
         }
-        return res;
+        return result;
+        
     }
 };
