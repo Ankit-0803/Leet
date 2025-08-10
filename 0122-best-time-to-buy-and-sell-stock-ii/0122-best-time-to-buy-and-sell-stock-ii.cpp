094f5bae88038c4f1dfe078c0220a72b254e527d
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-10 13:19:41 +0530

    Time: 1 ms (45.11%), Space: 17.1 MB (87.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
-
-        for (int i = 1; i < prices.size(); ++i) {
-            // If price is rising, collect the profit
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        int profit=0;
+        for(int i=1; i<prices.size(); i++){
+            // If today's price is higher than yesterday's
+            // we can take profit by buying yesterday & selling today
+            if(prices[i]>prices[i-1]){
+            profit+=prices[i]-prices[i-1];
             }
         }
-
         return profit;
+        
     }
 };
+// LeetCode’s “stock” problems are hypothetical.
+// They give you the whole list of prices beforehand.
+// The challenge is not forecasting, but finding the max possible profit if you had perfect knowledge of future prices.
