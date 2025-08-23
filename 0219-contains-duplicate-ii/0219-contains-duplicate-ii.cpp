3613ae4c9bd3d05139872ba47d5c8fe9b2390d1e
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-23 20:01:26 +0530

    Time: 75 ms (55.1%), Space: 98.6 MB (47.85%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // Hashmap to store the last index of each number
+        unordered_map<int, int> lastIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            // If num was seen before
+            if (lastIndex.count(num)) {
+                // Check distance
+                if (i - lastIndex[num] <= k) {
+                    return true; // Duplicate found within range k
+                }
+            }
+            // Update latest index of this number
+            lastIndex[num] = i;
+        }
+        return false; // No such duplicates found
+    }
+};
