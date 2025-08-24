36f4727df7ae4206574a01fd68b74232c49c3099
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-24 16:47:14 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,23 +3,20 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
         int count=1;
-        int candidate=nums[0];
+        int maj=nums[0];
         for(int i=1; i<n; i++){
-          if(nums[i]==candidate)count++;
-          else count--;
-          if(count==0){
-            candidate=nums[i];
-            count=1;
-          }
-        }return candidate;
-        
+            if(nums[i]==maj){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                maj=nums[i];
+                count=1;
+            }
+
+        }
+        return maj;
     }
 };
-// We use the Boyer–Moore Majority Vote Algorithm to find the majority element in O(n) time and O(1) space.
-// The algorithm has two main ideas:
-// Keep a candidate element and a count.
-// Traverse the array:
-// If the current element equals the candidate → increment count.
-// Else → decrement count.
-// If count becomes 0 → choose the current element as the new candidate and reset count to 1.
-// Since the majority element appears more than ⌊n/2⌋ times, it will survive as the candidate at the end.
