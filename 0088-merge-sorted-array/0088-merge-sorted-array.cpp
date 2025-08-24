a91db0e859ff8ea69a1072c3185940cdb62d1d3a
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-24 15:42:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (8.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1; //last element of nums1
-        int j=n-1; //last element of nums2
-        int k=m+n-1; 
-        while(i>=0 && j>=0){ //start traversing from the end of nums1 and filling the greater element through two pointer approach
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-            }
-            else{
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums2[j]>=nums1[i]){
                 nums1[k]=nums2[j];
                 j--;
             }
+            else
+            {
+            nums1[k]=nums1[i];
+            i--; 
+            }
             k--;
-            
         }
-        //if elements of nums2 are still remaining, directly add them in nums1
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
             k--;
         }
-        
     }
 };
