86ca3e3b420c78aefe80040efc032211a66a6141
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-24 19:40:54 +0530

    Time: 0 ms (100%), Space: 52.3 MB (34.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach=0; //farthest index we can reach so far
-        for(int i=0; i<nums.size(); i++){
-            if(i>maxReach)return false;
-            maxReach=max(maxReach, i+nums[i]);
+        int n=nums.size();
+        int maxreach=0;
+        for(int i=0; i<n; i++){
+            if(i>maxreach)return false;
+        maxreach=max(maxreach, i+nums[i]);
         }
-        return true; //loop completed successfully
+        return true;;
     }
 };
-// At i = 4, we find that the index 4 is beyond the farthest reachable position (maxReach = 3).
-// This means we cannot get to index 4, so we return false.
