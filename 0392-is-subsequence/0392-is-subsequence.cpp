e27de1e84ee51dc81793c923d2ea44cf69bb1c12
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-25 12:10:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.67%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        int m=s.size();
+        int n=t.size();
         int i=0, j=0;
-        while(i<s.size() && j<t.size()){
+        while(i<m && j<n){
             if(s[i]==t[j]){
                 i++;
-            };
-            j++;
+                j++;
+            }
+            else{
+                j++;
+            }
         }
-        //only return true if we reached the end of string s,
-        // that means all elements of string s are coverend
-        return i==s.size();
+        return i==m;
         
     }
 };
