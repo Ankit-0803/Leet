ecea696d7a63479bc8ae327d639e4c5a065c4de8
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-25 14:45:32 +0530

    Time: 0 ms (100%), Space: 11.8 MB (75.56%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<int, int>mpp;
+        //unordered_map<int, int>mpp;
+        int mpp[26]={0};
         for(char ch: magazine){
             mpp[ch-'a']++;        
             }
