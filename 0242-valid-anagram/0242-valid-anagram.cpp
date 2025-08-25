4c46ba5508d36525b8af17353bfe5d1ab29316a2
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-25 16:01:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return false;
-        unordered_map<int, int>mpp;
+        // unordered_map<int, int>mpp;
+        vector<int>mpp(26,0);//for lowercase letters
         for(char ch: s){
             mpp[ch-'a']++;
          }
          for(char ch:t){
             mpp[ch-'a']--;
          }
-         for(char ch:s){
-            if(mpp[ch-'a']!=0)return false;
+         for(int count: mpp){
+        if(count!=0)return false;
          }
          return true;
         
