3019d5496b08f39812336e28ef2b296a80527dc9
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-08-27 11:18:29 +0530

    Time: 0 ms (100%), Space: 77.2 MB (12.9%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,50 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // Create a dummy node to simplify result list building
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy; // Pointer to build the list
-        
-        int carry = 0; // Carry for addition
-        
-        // Loop while there is a node in l1 or l2 or carry is non-zero
-        while (l1 || l2 || carry) {
-            
-            // Get current values from both lists (or 0 if list ended)
-            int l1val = (l1 != nullptr) ? l1->val : 0;
-            int l2val = (l2 != nullptr) ? l2->val : 0;
-            
-            // Calculate sum for this digit
-            int sum = l1val + l2val + carry;
-            
-            // Update carry for next position
-            carry = sum / 10;
-            
-            // Create new node with the current digit (sum % 10)
-            temp->next = new ListNode(sum % 10);
-            
-            // Move temp pointer forward
-            temp = temp->next;
-            
-            // Move l1 and l2 forward if they are not null
-            if (l1) l1 = l1->next;
-            if (l2) l2 = l2->next;
+        ListNode* dummy= new ListNode(-1);
+        ListNode* temp=dummy;
+        int carry=0;
+        int sum=0;
+        while(l1 || l2 || carry){
+            int l1val=(l1!=nullptr)?l1->val:0;
+            int l2val=(l2!=nullptr)?l2->val:0;
+            int sum=l1val+l2val+carry;
+            carry=sum/10;
+            temp->next=new ListNode(sum%10);
+            if(l1)l1=l1->next;
+            if(l2)l2=l2->next;
+            temp=temp->next;
+
         }
-        
-        // Return the actual head of the result list
         return dummy->next;
+
+        
     }
 };
