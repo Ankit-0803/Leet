78601a85592eae840a81ea19e1a13d83df28f89e
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-09-11 12:57:15 +0530

    Time: 3 ms (24.51%), Space: 8.9 MB (82.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string result = "";    // To store the final binary sum
-        int i = a.size() - 1;  // Pointer for string a (starting from last digit)
-        int j = b.size() - 1;  // Pointer for string b
-        int carry = 0;         // To store carry during addition
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int carry=0;
+        string res="";
+        while(i>=0 || j>=0 ||carry){
+            int sum=carry;
+            if(i>=0)sum+=a[i--]-'0';
+            if(j>=0)sum+=b[j--]-'0';
+            res.push_back(sum%2+'0');
+            carry=sum/2;
 
-        // Loop until both strings are processed OR carry is left
-        while (i >= 0 || j >= 0 || carry) {
-            int sum = carry;   // Start with carry from previous step
-            if (i >= 0) sum += a[i--] - '0';  // Convert char to int and add
-            if (j >= 0) sum += b[j--] - '0';
-
-            result.push_back((sum % 2) + '0'); // Append the current bit
-            carry = sum / 2;                   // Update carry
         }
-        reverse(result.begin(), result.end()); // Since we added from right to left
-        return result;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
