1b335b77af5b52962778408adad43b5ed2c347be
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-09-13 01:11:39 +0530

    Time: 20 ms (94.17%), Space: 16 MB (96.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int rows, int cols){
-        int n=grid.size();
-        int m=grid[0].size();
-        //check bounds and skip water
-        if(rows<0 || rows>=n || cols<0 || cols>=m ||grid[rows][cols]=='0')return;
-        grid[rows][cols]='0'; //mark current land as water to avoid repeatation
-        //now traverse in all four directions of current land
+        int m=grid.size();
+        int n=grid[0].size();
+        if(rows<0 || rows>=m || cols<0 || cols>=n || grid[rows][cols]=='0'){
+            return;
+        }
+        grid[rows][cols]='0'; //mark current box 0 to avoid further confusion
+        //move in all four directions and mark them 0
         dfs(grid, rows+1, cols);
         dfs(grid, rows-1, cols);
         dfs(grid, rows, cols+1);
         dfs(grid, rows, cols-1);
+
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-           if(n==0)return 0;
-        int m=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
         int count=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
-                    count++;          //new island found
-                    dfs(grid, i, j); //mark all its part
+                    count++; //increase count by 1
+                    dfs(grid, i, j); //make all adjacent 1's included
                 }
             }
-        }return count;
+        }
+        return count;
     }
 };
