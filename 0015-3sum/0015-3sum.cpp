7fd5d0823d5ff41b13e682ac15026a1059832356
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-09-12 14:46:02 +0530

    Time: 54 ms (36.58%), Space: 29.1 MB (47.95%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,23 +7,27 @@ public:
         for(int i=0; i<n; i++){
             //skip duplicates for i
             if(i>0 && nums[i]==nums[i-1])continue;
-            int j=i+1, k=n-1;
+            int j=i+1;
+            int k=n-1;
             while(j<k){
             int sum=nums[i]+nums[j]+nums[k];
             if(sum==0){
                 res.push_back({nums[i], nums[j], nums[k]});
-            //skip duplicates for j and k
-            while(j<k && nums[j]==nums[j+1])j++;
-            while(j<k && nums[k]==nums[k-1])k--;
-            j++;
-            k--;
+                j++;
+                k--;
+            
+                while(j<k && nums[j]==nums[j-1]){j++;}
+                while(j<k && nums[k]==nums[k+1]){k--;}
+                
             }
-            else if(sum<0)j++;
-            else k--;
+            else if(sum<0){
+                j++;
+            }
+            else {
+                k--;
             }
         }
-        
+     }
         return res;
-        
     }
 };
