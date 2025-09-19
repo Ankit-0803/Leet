c766c2d58aef053b4b83e7d578d996f114af8be6
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-09-19 12:16:31 +0530

    Time: 0 ms (100%), Space: 38.2 MB (95.7%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+       int x=tasks.size();
+       vector<int>freq(26,0);
+       //count frequencies of all the tasks
+       for(char c:tasks){
+        freq[c-'A']++;
+       }
+       //find the task with max frequency 
+       int maxfreq=*max_element(freq.begin(), freq.end());
+       //count how many tasks have max frequency
+       int maxcount=count(freq.begin(), freq.end(), maxfreq);
+       int part1=(maxfreq-1)*(n+1)+maxcount;
+       return max(x,part1);
+
+     
+    }
+    };
