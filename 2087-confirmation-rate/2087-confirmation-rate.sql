eddeef0aed077c493f1c9bfab75d7b21c1b7781a
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-10-16 23:02:52 +0530

    Time: 572 ms (90.5%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -2,5 +2,5 @@
 select s.user_id, 
 round(coalesce(sum(case when action='confirmed' then 1 end)/count(*),0),2) as confirmation_rate
 from Signups s
-left join confirmations c on s.user_id=c.user_id
+left join Confirmations c on s.user_id=c.user_id
 group by user_id
