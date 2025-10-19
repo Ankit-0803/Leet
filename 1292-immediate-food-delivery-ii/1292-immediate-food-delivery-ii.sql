9c6a01b74893706b291263b1c17f86038396c362
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-10-19 13:51:04 +0530

    Time: 1210 ms (11.32%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 select round((count(case when d.order_date=d.customer_pref_delivery_date then 1 end)/count(*))*100, 2)immediate_percentage from delivery d
+# subquery to look at first order only
 where d.order_date=(
     select
     min(order_date) from delivery 
