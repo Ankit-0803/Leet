2a7397a79aacb0109b889172498cc98ef7946ca8
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-10-21 13:09:50 +0530

    Time: 48 ms (31.32%), Space: 54.2 MB (20.03%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,19 @@
+/**
+ * @return {Function}
+ */
+// var createHelloWorld = function() {
+    
+//     return function(...args) {
+//         return "Hello World";
+//     }
+// };
+var createHelloWorld=function(){
+    return function(){
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
