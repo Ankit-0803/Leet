9a86414bebda1407343f1ebdc28f0d75f4e12170
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-10-26 15:33:50 +0530

    Time: 119 ms (27.97%), Space: 80.7 MB (9.16%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const grouped={};
+    for(const item of this){
+        //get group key by calling fn on item
+        const key=fn(item);
+        //initilise group key if it doesn't exists
+        if(!grouped[key]){
+            grouped[key]=[];
+        }
+        //add items to the group
+        grouped[key].push(item);
+    }
+    return grouped;
+
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
