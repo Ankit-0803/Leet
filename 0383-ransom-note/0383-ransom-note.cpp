12e28f0c1b6cd71bbd1f8ca4d84c5872bfd01c86
Ankit <142109396+Ankit-0803@users.noreply.github.com>
2025-11-07 15:25:35 +0530

    Time: 0 ms (100%), Space: 11.8 MB (50.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,12 +3,14 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int>freq(26,0);
         for(char c: magazine){
-           freq[c-'a']++;
+            freq[c-'a']++;
         }
-        for(char c: ransomNote){
+        for(char c:ransomNote){
             freq[c-'a']--;
             if(freq[c-'a']<0)return false;
         }
         return true;
+    
+
     }
 };
